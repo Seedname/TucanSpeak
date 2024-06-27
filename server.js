8c8194d99581da9bbf7cb73933734ee952f4badb
Seedname <juliandominguez06@icloud.com>
2024-06-27 10:18:09 -0400

    added back translation

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ import { promisify } from 'util'
 import session from 'express-session';
 import { default as RedisStore } from "connect-redis";
 import { createClient } from 'redis';``
-// import { TranslationServiceClient } from '@google-cloud/translate';
+import { TranslationServiceClient } from '@google-cloud/translate';
 import path from 'path';
 
 
@@ -101,7 +101,6 @@ const wss = new WebSocketServer({
   verifyClient: (info, done) => {
     console.log("Parsing session info from request...");
     sessionParser(info.req, {}, () => {
-      // console.log(info.req);
       done(info.req.session);
     })
   }
@@ -208,7 +207,7 @@ function sign(val, secret){
 }
 
 let nextUpdateTime;
-// const translate = new TranslationServiceClient();
+const translate = new TranslationServiceClient();
 
 async function generatePrompts() {
   try {
@@ -235,21 +234,21 @@ async function generatePrompts() {
 
 async function translatePrompts(prompts, targetLanguage) {
   try {
-    // const [translations] = await translate.translateText({
-    //     parent: `projects/${process.env.GOOGLE_CLOUD_CONSOLE_PROJECT_ID}`,
-    //     contents: prompts,
-    //     mimeType: 'text/plain',
-    //     sourceLanguageCode: 'en',
-    //     targetLanguageCode: targetLanguage,
-    // });
+    const [translations] = await translate.translateText({
+        parent: `projects/${process.env.GOOGLE_CLOUD_CONSOLE_PROJECT_ID}`,
+        contents: prompts,
+        mimeType: 'text/plain',
+        sourceLanguageCode: 'en',
+        targetLanguageCode: targetLanguage,
+    });
 
-    // if (!Array.isArray(translations.translations)) {
-    //     throw new Error('Translations are not in expected format.');
-    // }
+    if (!Array.isArray(translations.translations)) {
+        throw new Error('Translations are not in expected format.');
+    }
 
-    // const translatedPrompts = translations.translations.map(translation => translation.translatedText);
+    const translatedPrompts = translations.translations.map(translation => translation.translatedText);
 
-    // return translatedPrompts;
+    return translatedPrompts;
   } catch (e) {
     console.error('Error translating prompts: ', e);
     return prompts;
